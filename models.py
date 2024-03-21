c8e0dedb87f07f7b5dc1bef05f7397bb194797a5
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-21 22:59:15 +0530

    defined username

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 class User(Model):
     id= fields.InteField(pk= True, index=True)
+    username = fields.CharField(max_length=20, null=False, Unique = True)
     
 
 
