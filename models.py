39d6d3d3d38549e57f416c283ed50ba6d24a920b
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-21 23:01:13 +0530

    defined email

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 class User(Model):
     id= fields.InteField(pk= True, index=True)
     username = fields.CharField(max_length=20, null=False, Unique = True)
-    
+    email=fields.CharField(max_length=200, null=False, Unique = True)
+
 
 
