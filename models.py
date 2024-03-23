65cbf464c73fd18c92dc722998b00684a56d31b5
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-23 12:41:47 +0530

    defined region

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class Business(Model):
      id= fields.InterField(pk= True, index=True)
      business_name = fields.CharField(max_length=20, null=False, Unique = True)
      city=fields.CharField(max_length=100, null=False, default="Unspecified")
-     
+     region=fields.CharField(max_length=100, null=False, default="Unspecified")
 
 
 
