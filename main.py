1916d498266e83dd03ca490cbec37a9545d87037
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-27 22:58:46 +0530

    sqlite database is ready

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from tortoise.contrib.fastapi import register_tortoise
 from models import *
 
 app= FastAPI()
+
+
+register_tortoise(
+    app,
+    db_url="sqlite://database.sqlite3",
+    models={"models": ["models"]}
+)
