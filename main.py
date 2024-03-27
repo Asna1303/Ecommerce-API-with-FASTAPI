327985cd7b89f915d62be021cbe13e1a8ea317b9
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-27 23:15:27 +0530

    installed fastapi

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
 register_tortoise(
     app,
     db_url="sqlite://database.sqlite3",
-    models={"models": ["models"]}
+    models={"models": ["models"]},
     generate_schemas=True,
     add_exception_handlers=True
 
