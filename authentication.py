c309682f0715064fcfdfc37dffb34769722e6e68
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-29 16:27:19 +0530

    returned the hash string of password text

--- authentication.py
+++ authentication.py
@@ -3,4 +3,4 @@ from passlib.context import CryptContext
 
 pwd_context=CryptContext(schemes=['bcrypt'],deprecated ='auto')
 def get_hashed_password(password):
-    
+    return pwd_context.hash(password)
