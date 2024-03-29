9763f214d1ae9ab2c918b941c746999d53491296
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-29 16:29:12 +0530

    imported everything from authentication

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI;
 from tortoise import models
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
+from authentication import *
 
 app= FastAPI()
 
